5a423fcc3e01f2d649a831e7d6d1bbe247ce0d43
heather <>
2007-10-24 19:12:03 +0000

    add some release.notes and mainpage.h

---
+++ src/mainpage.h
@@ -0,0 +1,15 @@
+//$Header: /nfs/slac/g/glast/ground/cvs/GlastPolicy/src/mainpage.h,v 1.15 2004/05/12 16:42:34 hansl Exp $
+
+/*! @mainpage  package gr_app 
+
+
+<hr>
+  \section requirements requirements
+  \include requirements
+
+<hr>
+    @todo  
+    <ul> <li> Introduce use of facilities and start using commonUtilities 
+    </ul>
+
+*/
